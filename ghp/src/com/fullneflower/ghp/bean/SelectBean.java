9d058c66a37bb62d74ec7095241bba52348cb838
Owner <Owner@AA774>
2015-06-17 17:43:11 +0900

    やるぞ～

---
+++ ghp/src/com/fullneflower/ghp/bean/SelectBean.java
@@ -0,0 +1,16 @@
+package com.fullneflower.ghp.bean;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fullneflower.ghp.exception.GhpException;
+
+public class SelectBean implements  FullneflowerBean{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws GhpException {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
